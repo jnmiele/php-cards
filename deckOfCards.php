c82cec2e5b15267fdab5fc3a463a8323af77333c
github username <github email address>
2017-12-01 15:55:41 -0500

    wrote playGame(). function instantiates a new deck, shuffles, and deals 5 cards to each player.

--- deckOfCards.php
+++ deckOfCards.php
@@ -13,24 +13,24 @@ class Deck
   
   public function __construct()
   {
-    $this->cards = array();
+    $this->cards = [];
     $suits = array('Clubs', 'Diamonds', 'Hearts', 'Spades');
     $ranks = array('Ace', 'King', 'Queen', 'Jack', '2', '3', '4', '5', '6', '7', '8', '9', '10');
     foreach ($suits as $suit) {
       foreach ($ranks as $rank) {
         $card = new Card($rank, $suit);
-        array_push($this->cards, $card);
+        $this->cards[] = $card;
       }
     }
   }
   
   public function shuffleCards()
   {
-    $shuffled = array();
+    $shuffled = [];
     while (count($this->cards) > 0):
       $index = array_rand($this->cards);
       $card = array_splice($this->cards, $index, 1)[0];
-      array_push($shuffled, $card);
+      $shuffled[] = $card;
     endwhile;
     $this->cards = $shuffled;
   }
@@ -46,17 +46,58 @@ class Deck
 class Player 
 {
   
-  protected $hand;
+  public $hand;
   
   public function __construct()
   {
-    $this->hand = array();
+    $this->hand = [];
   }
   
   public function showHand()
   {
-    foreach ($this->hand as $card) {
-      echo $card->rank . ' of ' . $card->suit;
+    if (count($this->hand) > 0) {
+      foreach ($this->hand as $card) {
+        echo $card->rank . ' of ' . $card->suit . "\n";
+      }
+      echo "___________________ \n\n";
+    } else {
+    echo "no cards here bro\n";   
     }
   }
 }
+
+function dealCards($cards, $players) {
+  foreach ($players as $player) {
+    while (count($player->hand) !== 5):
+      $card = array_splice($cards, 0, 1)[0];
+      $player->hand[] = $card;
+    endwhile;
+  }
+}
+
+function createPlayers($array, $n) {
+  while (count($array) !== $n):
+    $p = new Player();
+    array_push($array, $p);
+  endwhile;
+  return $array;
+}
+
+function playGame(){
+  $deck = new Deck();
+  $deck->shuffleCards();
+  $players = [];
+  $players = createPlayers($players, 4);
+  $communityCards = [];
+
+  dealCards($deck->cards, $players);
+  
+  foreach ($players as $p) {
+    $p->showHand();
+  }
+  
+}
+
+playGame();
+
+
